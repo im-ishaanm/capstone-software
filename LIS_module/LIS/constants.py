6187a34d1e57bcddb09e265bb814ef304d725b0c
Ishaan M <55643770+im-ishaanm@users.noreply.github.com>
2022-12-16 11:14:00 +0530

    Update constants.py

--- LIS_module/LIS/constants.py
+++ LIS_module/LIS/constants.py
@@ -1,9 +1,9 @@
 # URLs
 
 # URL to fetch data and get sample details
-FETCH_URL = "https://reports.anandlab.com/listest/samplesorter.asmx/GetSampleDetails"
+FETCH_URL = ""
 
 # URL to update the database if the sample has been processed
-UPDATE_URL = "https://reports.anandlab.com/listest/samplesorter.asmx/UpdateSampleReciveStatus"
+UPDATE_URL = ""
 
-API_KEY = "691038ED-6C2C-499A-BE9E-F8EE5807AFD0"
+API_KEY = ""
